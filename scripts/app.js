3b713498aa9ce0f185debab4ae9643a9c7607940
saleh <ghalayinisaleh9@gmail.com>
2024-12-19 17:11:48 +0200

    edited some of the input attributes

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,22 @@
-const toggle = document.getElementById("toggle");
-const result = document.getElementById("result");
-const containers = document.querySelectorAll(".container");
-const calc_btn = document.querySelectorAll(".calc-btn");
 const body = document.body;
+const field = document.getElementById("field");
+const toggle = document.getElementById("toggle");
+const calc_btn = document.querySelectorAll(".calc-btn");
+const containers = document.querySelectorAll(".container");
 
 toggle.addEventListener("click", () => {
     body.classList.toggle("light-body");
+    toggle.classList.toggle("light-toggle");
+    field.classList.toggle("light-content");
+
     containers.forEach(cont => {
         cont.classList.toggle("light-container");
     });
-    result.classList.toggle("light-content");
+
     calc_btn.forEach(btn => {
         btn.classList.toggle("light-content");
     });
-    toggle.classList.toggle("light-toggle");
 });
+
+
+
