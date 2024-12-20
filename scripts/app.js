7b8ad9209767391170fac3fbbfed46c6f87c214f
saleh <ghalayinisaleh9@gmail.com>
2024-12-20 03:34:30 +0200

    created a function called applyError that takes the type of error as a code and prints a message for each kind of error and applys it to the process card while keeping the equation unchanged

--- scripts/app.js
+++ scripts/app.js
@@ -15,6 +15,7 @@ const field = document.getElementById("field");
 const equal = document.getElementById("equal");
 const toggle = document.getElementById("toggle");
 const title = document.querySelectorAll(".title");
+const pro_title = document.getElementById("process-card");
 const process_div = document.getElementById("process");
 const buttons = document.querySelectorAll(".calc-btn");
 const containers = document.querySelectorAll(".container");
@@ -71,74 +72,77 @@ function evaluatePrefix(list) {
 }
 
 function evaluatePostfix(list) {
-        stack = [];
-        for(let i = 0; i < list.length; i++) {
-            if(!isNaN(list[i]))
-                stack.push(Number(list[i]));
-            else if (operators.includes(list[i])) {
-                op_two = stack.pop();
-                op_one = stack.pop();
-                result = performOperation(list[i], op_one, op_two);
-                solving_steps.push(`Evaluate ${op_two} ${op_one} ${list[i]} = 
-                    <br />${op_two} ${list[i]} ${op_one} = 
-                    <br />${Number.isInteger(result) ? result : result.toFixed(2)}`);
-                stack.push(result);
-            }
+    stack = [];
+    for(let i = 0; i < list.length; i++) {
+        if(!isNaN(list[i]))
+            stack.push(Number(list[i]));
+        else if (operators.includes(list[i])) {
+            op_two = stack.pop();
+            op_one = stack.pop();
+            result = performOperation(list[i], op_one, op_two);
+            solving_steps.push(`Evaluate ${op_two} ${op_one} ${list[i]} = 
+                <br />${op_two} ${list[i]} ${op_one} = 
+                <br />${Number.isInteger(result) ? result : result.toFixed(2)}`);
+            stack.push(result);
         }
-        displayResult(result);
-        updateProcess();
     }
+    displayResult(result);
+    updateProcess();
+}
+
+function applyError(e_code) {
+    pro_title.classList.add("error-style");
+    field.focus();
+
+    if(e_code === 1)
+        return pro_title.innerHTML = `You haven't entered anything to evaluate!`;
+    else if (e_code === 2)
+        return pro_title.innerHTML = `You haven't entered enough input to evaluate!`;
+    else if (e_code === 3)
+        return pro_title.innerHTML = `You have entered an invalid input!`;
+    else if (e_code === 4)
+        return pro_title.innerHTML = `The number of operators and operands is not proportional`;
+    else if (e_code === 5)
+        return pro_title.innerHTML = `This expression is invalid for a postfix evaluation!`;
+    else if (e_code === 6)
+        return pro_title.innerHTML = `This expression is invalid for a prefix evaluation!`;
+    else if (e_code === 7)
+        return pro_title.innerHTML = `The last character in a postfix expression must be an operator!`;
+    else if (e_code === 8)
+        return pro_title.innerHTML = `The last character in a prefix expression must be a number!`;
+}
 
 function checkEquation(eq) {
     operand_counter = 0;
     operator_counter = 0;
     eq_list = eq.trim().split(" ");
     
-    if(eq.trim() == "") {
-        alert("You haven't entered anything to evaluate!");
-        field.focus();
-        return;
-    } else if (eq_list.length < 3) {
-        alert("You haven't entered enough input to evaluate!");
-        field.focus();
-        return;
-    }
-
-
+    if(eq.trim() == "")
+       return applyError(1);
+    else if (eq_list.length < 3) 
+        return applyError(2);
     for(let i = 0; i < eq_list.length; i++) {
-        if (!operators.includes(eq_list[i]) && isNaN(eq_list[i])) {
-            alert("You have entered an invalid input!");
-            field.focus();
-            return;
-        }
+        if (!operators.includes(eq_list[i]) && isNaN(eq_list[i]))         
+            return applyError(3);
         if (operators.includes(eq_list[i]))
             operator_counter++;
         else if (!isNaN(eq_list[i]))
             operand_counter++;
     }
     
-    if (operator_counter !== operand_counter - 1) {
-        alert("The number of operators and operands is not proportional");
-        return;
-    }
+    if (operator_counter !== operand_counter - 1)
+        return applyError(4);
     
-    if(isNaN(eq_list[0]) && type.innerHTML === "Postfix") {
-        alert("This expression is invalid for a postfix evaluation!");
-        return;
-    } else if (!isNaN(eq_list[0]) && type.innerHTML === "Prefix") {
-        alert("This expression is invalid for a prefix evaluation!");
-        return;
-    }
+    if(isNaN(eq_list[0]) && type.innerHTML === "Postfix")
+        return applyError(5);
+    else if (!isNaN(eq_list[0]) && type.innerHTML === "Prefix")
+        return applyError(6);
     if (type.innerHTML === "Postfix") {
-        if (!operators.includes(eq_list[eq_list.length - 1])) {
-            alert("The last character in a postfix expression must be an operator!");
-            return;
-        }
+        if (!operators.includes(eq_list[eq_list.length - 1]))
+            return applyError(7);
     } else if (type.innerHTML === "Prefix") {
-        if (isNaN(eq_list[eq_list.length - 1])) {
-            alert("The last character in a prefix expression must be a number!");
-            return;
-        }
+        if (isNaN(eq_list[eq_list.length - 1]))
+            return applyError(8);
     }
 
     if (operators.includes(eq_list[0]))
